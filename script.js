6d56f1021340206ae3d2e401728b5950e9755113
Kwiasek <86431269+Kwiasek@users.noreply.github.com>
2023-02-16 20:52:45 +0100

    Made script to toggle a color class every time mouse hovers a tile.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let container = document.querySelector('.grid');
 
-
-
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
-    div.classList.add('tile')
-    div.textContent = ''
+    div.classList.add('tile');
     container.appendChild(div);
 }
+
+
+let tiles = document.querySelectorAll('.tile');
+
+tiles.forEach(tile => {
+    tile.addEventListener('mouseover', () => {
+        tile.classList.toggle('hovered')
+    })
+})
+
+
