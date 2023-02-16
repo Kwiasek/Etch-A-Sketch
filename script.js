20efcbb0d1625e63b08812f10e14f5fbc5b3d231
Kwiasek <86431269+Kwiasek@users.noreply.github.com>
2023-02-16 21:32:38 +0100

    Added function to change size of canvas

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 let container = document.querySelector('.grid');
+let tiles;
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('tile');
-    container.appendChild(div);
+const eventMouse = () => {
+    tiles.forEach(tile => {
+        tile.addEventListener('mouseover', () => {
+            tile.style.background = randColor();
+    })
+})}
+
+const canvas = (size) => {
+    for (let i = 0; i < (size * size); i++) {
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        let div = document.createElement('div');
+        div.classList.add('tile');
+        container.appendChild(div);
+    }
+    tiles = document.querySelectorAll('.tile');
+    eventMouse();
+}
+
+canvas(16); //Default 16x16 canvas
+
+document.querySelector('#button').addEventListener('click', () => {
+    let size = prompt('Give a size of your new canvas, input number of rows, columns will be equal to them.');
+    size = parseInt(size);
+    if (size > 100) {
+        alert('Max size is 100x100');
+    }
+    else if (size < 1) {
+        alert('Give a proper size');
+    }
+    else {
+        container.innerHTML = '';
+        canvas(size);
+    }
+})
+
+const randColor = () => {
+    let r = Math.floor(Math.random() * 257);
+    let g = Math.floor(Math.random() * 257);
+    let b = Math.floor(Math.random() * 257);
+
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 
-let tiles = document.querySelectorAll('.tile');
 
-tiles.forEach(tile => {
-    tile.addEventListener('mouseover', () => {
-        tile.classList.toggle('hovered')
-    })
-})
 
 
